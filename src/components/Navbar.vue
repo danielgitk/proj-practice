4775d56183334bf5ca6aa5fbb40d97ba7e6207c3
Daniel <daniel@localhost.localdomain>
2021-07-30 22:35:11 +0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+      <v-app-bar app flat>
+   <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            drawer : false
+                    }
+    
+
+},
+}
+</script>
+
+<style>
+
+</style>
